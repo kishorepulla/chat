b6f29152c57f842a44d275a7b475978e6305d326
kishorepulla <kishoreaug20@gmail.com>
2016-12-28 08:41:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,23 @@ var io = require('socket.io').listen(server);
 	
 server.listen(process.env.PORT || 3000);
 
-
+var clientCount = 0;
 var messages_ = [];
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function(socket){
+	clientCount++;
 	socket.emit('init messages', messages_);
 	socket.on('chat message', function(msg){
 	messages_.push(msg);
 	io.emit('chat message', msg);
+	});
+	socket.on('disconnect', function(){
+		clientCount--;
+		if(clientCount <= 0){
+			messages_ = [];
+		}
 	});
 });
